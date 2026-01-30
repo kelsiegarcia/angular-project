688034d1c17919e3d79c296c39cc73cc26d3a6c8
Kelsie Garcia <kelsie.c.garcia@gmail.com>
2026-01-30 09:49:33 -1000

    added document item and document list

--- src/app/documents/document-list/document-list.ts
+++ src/app/documents/document-list/document-list.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../documents.model';
 
 @Component({
   selector: 'cms-document-list',
   standalone: false,
   templateUrl: './document-list.html',
-  styleUrl: './document-list.css',
+  styleUrls: ['./document-list.css'],
 })
 export class DocumentList {
+  documents: Document[] = [];
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  constructor() {
+    this.documents = [
+      { id: '1', name: 'Document 1', description: 'Description A', url: 'www.example.com/doc1', children: [] },
+      { id: '2', name: 'Document 2', description: 'Description B', url: 'www.example.com/doc2', children: [] },
+      { id: '3', name: 'Document 3', description: 'Description C', url: 'www.example.com/doc3', children: [] }
+    ];
+  }
+
+  onSelected(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 
 }
